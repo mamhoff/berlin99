d97f7aa827f6362cd4a8d846590b41b38269b937
Alex <alex@demetriades.co.uk>
2014-03-05 17:38:23 +0100

    fuck this

--- header.php
+++ header.php
@@ -15,8 +15,6 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<script src="<?php echo get_template_directory_uri() . '/js/modernizr.custom.js'; ?>"></script>
-
 <?php wp_head(); ?>
 
 <meta charset="utf-8">
@@ -30,7 +28,7 @@
   <link href="apple-touch-icon.png" rel="apple-touch-icon">
 
   
-
+<script src="<?php echo get_template_directory_uri() . '/js/modernizr.custom.js'; ?>"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=&sensor=false&extension=.js"></script>
     
 
